432ed915896dbca4badccab3e45ada9fc2d91b3a
Hannes Hörl <hhorl@pivotal.io>
2022-05-13 09:39:50 +0200

    Move over to manage pkg versions with branches/tags

--- prep-ns.sh
+++ prep-ns.sh
@@ -5,28 +5,47 @@ set -u
 set -o pipefail
 
 readonly DIR="$( cd "$(dirname "${BASH_SOURCE[0]}")" && pwd )"
-readonly OVERLAY="${DIR}/ns.rbac.overlay.yml"
+readonly OVERLAY='ns.rbac.overlay.yml'
+
+getFileFromVersion() {
+    local repoPath="$1"
+    local ver="$2"
+
+    if [[ "$ver" == "-" ]] ; then
+        cat "${repoPath}"
+        return
+    fi
+
+    git show "${ver}:${repoPath}"
+}
+
+maybeApply() {
+    if test -t 1 ; then
+        kubectl apply -f -
+    else
+        cat
+    fi
+}
 
 main() {
     local pkg="${1?must be the app/pkg we are preparing for, one of those in ${DIR}/pkgs}"
-    local ver="${2?must be the app\'s/pkg\'s version}"
-    local name="${3?must be the app\'s/pkg\'s instance name}"
-    local ns="${4?must be the app\'s/pkg\'s namespace}"
+    local name="${2?must be the app\'s/pkg\'s instance name}"
+    local ns="${3?must be the app\'s/pkg\'s namespace}"
+    local ver="${4?must be the app\'s/pkg\'s version, can be - for the currently checked out revision}"
 
-    local objFile="${DIR}/pkgs/${pkg}/${ver}/ns.rbac.yml"
+    local rbacFile="pkgs/${pkg}/ns.rbac.yml"
 
-    [ -e "$objFile" ] || {
-        echo >&2 "no namespace/rbac config for package '$pkg/$ver' found, expected it in '$objFile'"
-        return 1
-    }
+    cd "${DIR}"
 
-    ytt -f "$objFile" -f "${OVERLAY}" --data-values-env 'DV' --data-value "instance=${name}" --data-value "ns=${ns}" | {
-        if test -t 1 ; then
-            kubectl apply -f -
-        else
-            cat
-        fi
-    }
+    {
+        getFileFromVersion "${rbacFile}" "${ver}"
+        echo '---'
+        getFileFromVersion "${OVERLAY}" "${ver}"
+    } | ytt -f - \
+        --data-values-env 'DV' \
+        --data-value "instance=${name}" \
+        --data-value "ns=${ns}" \
+      | maybeApply
 }
 
 main "$@"
--- updateRepo.sh
+++ updateRepo.sh
@@ -5,43 +5,86 @@ set -u
 set -o pipefail
 
 readonly HERE="$( cd "$(dirname "${BASH_SOURCE[0]}")" && pwd )"
-readonly PKG_DIR="${HERE}/pkgs"
-readonly REPO_DIR="${HERE}/repo"
+readonly PKG_DIR="pkgs"
+readonly REPO_DIR="repo"
 
 # if you want to clone the whole thing to a different repo:
 readonly REPO="${REPO:-https://github.com/hoegaarden/pkg}"
-readonly REPO_REF="${REPO_REF:-main}"
+
+currentTip() {
+    git rev-parse --abbrev-ref HEAD
+}
+
+getPkgRevs() {
+    git tag -l "${1}@*"
+    git branch -l "${1}@*"
+}
+
+getVersionedFile() {
+    local repoPath="$1"
+    local rev="${2:-}"
+
+    git show "${rev}:${repoPath}"
+}
+
+getCommitDate() {
+    git show -s --format=%cI "${1}"
+}
+
+genPkgForVersion() {
+    local pkgDir="$1"
+    local gitRev="$2"
+    local ver="$3"
+    local meta="$4"
+
+    getVersionedFile "${pkgDir}/pkg.yml" "$gitRev" \
+        | ytt \
+            --data-values-file "$meta" \
+            -v version="${ver}" \
+            -v repoPath="${pkgDir}/src" \
+            -v repo="${REPO}" \
+            -v repoRef="${gitRev}" \
+            -v date="$( getCommitDate "$gitRev" )" \
+            --data-values-file <(
+                getVersionedFile "${pkgDir}/src/values.yml" "$gitRev" | ytt -f - --data-values-schema-inspect -o openapi-v3
+            ) \
+            -f -
+}
 
 handlePkg() {
     local pkgName="$1"
 
+    local currentTip
+    currentTip="$( currentTip )"
     local pkgDir="${PKG_DIR}/${pkgName}"
     local repoDir="${REPO_DIR}/packages/${pkgName}"
-    local meta="${pkgDir}/meta.yml"
-
     mkdir -p "${repoDir}"
-    echo "## ${pkgName}: writing metadata"
-    cp "$meta" "${repoDir}/meta.yml"
 
-    local ver
-    while read -d $'\0' -r ver
+    # get the meta from the current tip
+    local meta="${pkgDir}/meta.yml"
+    echo "## ${pkgName}: writing package meta data"
+    getVersionedFile "$meta" "$currentTip" > "${repoDir}/meta.yml"
+
+    local rev ver
+    while read -r rev
     do
-        echo "## ${pkgName}: writing package for version ${ver}"
-        ytt \
-            --data-values-file "$meta" \
-            -v version="${ver}" \
-            -v repoPath="pkgs/${pkgName}/${ver}/src" \
-            -v repo="${REPO}" \
-            -v repoRef="${REPO_REF}" \
-            --data-values-file <(
-                ytt -f "${pkgDir}/${ver}/src/values.yml" --data-values-schema-inspect -o openapi-v3
-            ) \
-            -f "${pkgDir}/${ver}/pkg.yml" \
+        ver="${rev#*@}"
+        echo "## ${pkgName}/${ver}: writing package from ${rev} (verison from git revision)"
+        genPkgForVersion "$pkgDir" "$rev" "$ver" "$meta" \
             > "${repoDir}/${ver}.yml"
-    done < <(find "$pkgDir" -mindepth 1 -maxdepth 1 -type d -printf '%P\0')
+    done < <(getPkgRevs "$pkgName")
+
+    # the current tip as additional "dev" version
+    local versionFile="${pkgDir}/next_version"
+    ver="$( getVersionedFile "$versionFile" "$currentTip" )"
+    echo "## ${pkgName}/${ver}: writing package from ${currentTip} (version from $versionFile)"
+    genPkgForVersion "$pkgDir" "$currentTip" "$ver" "$meta" \
+        > "${repoDir}/${ver}.yml"
 }
 
 main() {
+    cd "$HERE"
+
     local pkgName
 
     while read -d $'\0' -r pkgName

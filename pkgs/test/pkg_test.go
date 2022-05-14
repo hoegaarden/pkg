c7d0b8eb4f5c56a1b3243d526f132cf3ed823fdf
Hannes Hörl <hhorl@pivotal.io>
2022-05-14 16:41:50 +0200

    test: New version with even more new features

--- pkgs/test/pkg_test.go
+++ pkgs/test/pkg_test.go
@@ -7,35 +7,62 @@ import (
 )
 
 func TestPackage(t *testing.T) {
-	commonTests := func(jq *JQer) {
-		jq.
-			IsNum("length", 2).
+	t.Parallel()
+
+	t.Run("with no data values, with defaults", func(t *testing.T) {
+		NewJQer(t, Ytt{}).
 			IsTrue(`.[0] | (.kind == "ConfigMap" and .apiVersion == "v1" and .data.some == "data")`).
 			IsTrue(`.[1] | (.kind == "Secret" and .apiVersion == "v1" and (.data.some|@base64d) == "data" )`).
-			IsYamlString(".[0].data.foo")
-	}
-
-	jq := NewJQer(t, Ytt{})
-	jq.
-		IsString(".[0].metadata.name", "something").
-		IsString(".[1].metadata.name", "something").
-		IsString(".[0].data.foo", "name: something\n")
-	commonTests(jq)
+			IsYamlString(".[0].data.foo").
+			IsString(".[0].metadata.name", "cm").
+			IsString(".[1].metadata.name", "sec").
+			IsString(".[0].data.foo", "secretName: sec\nconfigMapName: cm\n")
+	})
 
 	t.Run("with data values", func(t *testing.T) {
-		ytt := Ytt{
-			DVs: []DV{FileDV(
-				"#@data/values",
-				"---",
-				"name: some-random-name",
-			)},
+		t.Parallel()
+
+		testCases := map[string]struct {
+			dvs  []DV
+			test func(*JQer)
+		}{
+			"setting configMapName": {
+				dvs: []DV{PlainDV("configMapName", "some-cm")},
+				test: func(jq *JQer) {
+					jq.IsString(".[0].metadata.name", "some-cm").
+						MatchesString(".[0].data.foo", "configMapName: some-cm")
+				},
+			},
+			"setting secretName": {
+				dvs: []DV{PlainDV("secretName", "some-secret")},
+				test: func(jq *JQer) {
+					jq.IsString(".[1].metadata.name", "some-secret").
+						MatchesString(".[0].data.foo", "secretName: some-secret")
+				},
+			},
+			"setting secretName and configMapName": {
+				dvs: []DV{FileDV(
+					"#@data/values",
+					"---",
+					"configMapName: some-cm",
+					"secretName: some-secret",
+				)},
+				test: func(jq *JQer) {
+					jq.IsString(".[1].metadata.name", "some-secret").
+						MatchesString(".[0].data.foo", "secretName: some-secret").
+						MatchesString(".[0].data.foo", "configMapName: some-cm")
+				},
+			},
 		}
 
-		jq := NewJQer(t, ytt)
-		jq.
-			IsString(".[0].metadata.name", "some-random-name").
-			IsString(".[1].metadata.name", "some-random-name").
-			IsString(".[0].data.foo", "name: some-random-name\n")
-		commonTests(jq)
+		for name, tc := range testCases {
+			tc := tc
+			t.Run(name, func(t *testing.T) {
+				t.Parallel()
+
+				jq := NewJQer(t, Ytt{DVs: tc.dvs})
+				tc.test(jq)
+			})
+		}
 	})
 }
